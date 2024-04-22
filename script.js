ec2a4917bd9c27628077c06430cfe8513d622d27
angelaanjorin <163406589+angelaanjorin@users.noreply.github.com>
2024-04-22 13:37:26 +0200

    Add the const elements and add eventlisteners

--- script.js
+++ script.js
@@ -3,5 +3,34 @@
 let todo = JSON.parse(localStorage.getItem("todo")) || [];
 
 const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todoList");
+const todoCount = document.getElementById("todoCount");
+const addButton = document.querySelector(".btn");
+const deleteButton = document.getElementById("deleteButton");
 
-console.log(todoInput);
+// Initialize 
+
+document.addEventListener("DOMContentLoaded", 
+function (){
+    addButton.addEventListener("click", addTask);
+    todoInput.addEventListener('keydown', function(event) {
+        if(event.key === "Enter") {
+            event.preventDefault()
+            addTask();
+        }
+    });
+    deleteButton.addEventListener("click", deleteAllTasks);
+    displayTasks();
+});
+
+function addTask() {
+
+}
+
+function deleteAllTasks() {
+
+}
+
+function displayTasks() {
+
+}
